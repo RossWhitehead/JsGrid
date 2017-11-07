a67e85ec3798a59e94fd1bfd18c325d71743ede4
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2017-11-07 19:18:41 +0000

    Add string.js

--- jsGrid.js
+++ jsGrid.js
@@ -1,23 +1,37 @@
 /// <reference path="node_modules/jquery/dist/jQuery.js" />
+/// <reference path="string.js" />
 
 (function ($) {
 
+    const TEMPLATE = {
+        TABLE: '<table>',
+        THEAD: '<thead>',
+        TBODY: '<tbody>',
+        TH: '<th>{0}</th>',
+        TR: '<tr>',
+        TD: '<td>{0}</td>',
+        PAGE: '<span>Page {0}</span>'
+    };
+
     $.fn.jsGrid = function (config) {
         $.each(this, function () {
-            var columnCount = 3;
+            var columnCount = config.columns.length;
             var rowCount = 2;
-            var $table = $("<table>");
-            var $thead = $("<thead>");
-            var $tbody = $("<tbody>");
+            var $table = $(TEMPLATE.TABLE);
+            var $thead = $(TEMPLATE.THEAD);
+            var $tbody = $(TEMPLATE.TBODY);
 
             for (col = 0; col < columnCount; col++) {
-                $thead.append("<th>" + config.columns[col] + "</th>");
+                $thead.append(String.format(TEMPLATE.TH, config.columns[col]));
             }
 
+            var page = 1;
+            var offset = page * config.pageSize;
+
             for (row = 0; row < rowCount; row++) {
-                var $row = $("<tr>");
+                var $row = $(TEMPLATE.TR);
                 for (col = 0; col < columnCount; col++) {
-                    $row.append("<td>" + config.data[row][col] + "</td>");
+                    $row.append(String.format(TEMPLATE.TD, config.data[row][col]));
                 }
                 $tbody.append($row);
             }
@@ -25,6 +39,8 @@
             $table.append($thead).append($tbody);
 
             $(this).append($table);
+
+            $(this).append(String.format(TEMPLATE.PAGE, page));
         });
     };
 
