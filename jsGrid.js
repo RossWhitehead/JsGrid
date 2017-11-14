5a18e9b72affa919752573748b7f606e7d0749c1
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2017-11-14 12:43:15 +0000

    Refactor render methods

--- jsGrid.js
+++ jsGrid.js
@@ -3,7 +3,7 @@
 
 (function ($) {
 
-    var defaultConfig = {
+    const defaultConfig = {
         paging: {
             enabled: false,
             pageSize: 2
@@ -14,75 +14,106 @@
     const nextButtonId = 'next-button';
 
     const TEMPLATE = {
-        TABLE: '<table>',
+        TABLE: '<table class="table">',
         THEAD: '<thead>',
         TBODY: '<tbody>',
         TH: '<th>{0}</th>',
         TR: '<tr>',
         TD: '<td>{0}</td>',
         PAGE: '<span>Page {0}</span>',
+        PAGING_BUTTONS: '<div>',
         NEXT_BUTTON: String.format('<button id="{0}">Next</button>', nextButtonId),
         PREVIOUS_BUTTON: String.format('<button id="{0}">Previous</button>', previousButtonId)
     };
 
+    var $container = null;
+    var $table = $(TEMPLATE.TABLE);
+    var $thead = $(TEMPLATE.THEAD)
+    var $tbody = $(TEMPLATE.TBODY);
+    var $pagingButtons = $(TEMPLATE.PAGING_BUTTONS);
+
     $.fn.jsGrid = function (config) {
-        $.extend( config, defaultConfig );
-        renderGrid(config, $(this), 0);
-        
+        $.extend(config, defaultConfig);
+        initialize(config, $(this), 0);
     };
 
-    function renderGrid(config, $grids, page) {
+    function initialize(config, $grids, page) {
+        const rowCount = config.data.length;
+
         $.each($grids, function () {
-            var $this = $(this);
-            const columnCount = config.columns.length;
-            const rowCount = config.data.length;
-            var $table = $(TEMPLATE.TABLE);
-            var $thead = $(TEMPLATE.THEAD);
-            var $tbody = $(TEMPLATE.TBODY);
+            $container = $(this);
 
-            $this.empty();
-            $this.unbind();
+            // Make sure that the previous guests have left the room before renting it out.
+            $container.empty();
+            $container.unbind();
 
-            for (col = 0; col < columnCount; col++) {
-                $thead.append(String.format(TEMPLATE.TH, config.columns[col]));
-            }
+            // Render header
+            renderHeader(config);
+            $table.append($thead);
 
-            var pageStartRow = page * config.paging.pageSize;
-            var pageEndRow = Math.min(pageStartRow + config.paging.pageSize, rowCount);
+            // Render body
+            var page = 0;           
+            renderBody(config, page);
+            $table.append($tbody);
+            
+            $container.append($table);
 
-            for (row = pageStartRow; row < pageEndRow; row++) {
-                var $row = $(TEMPLATE.TR);
-                for (col = 0; col < columnCount; col++) {
-                    $row.append(String.format(TEMPLATE.TD, config.data[row][col]));
-                }
-                $tbody.append($row);
-            }
+            // Render the paging buttons
+            renderPagingButtons(config, page);
+            $container.append($pagingButtons);
 
-            $table.append($thead).append($tbody);
-
-            $this.append($table);
-
-            $this.append(String.format(TEMPLATE.PAGE, page + 1));
-
-            // Paging buttons
-            if (page > 0) {
-                var $previousButton = $(TEMPLATE.PREVIOUS_BUTTON);
-                $this.on('click', String.format('#{0}', previousButtonId), function () {
-                    renderGrid(config, $this, page - 1);
-                });
-
-                $this.append($previousButton);
-            }
-
-            if (rowCount > pageEndRow) {
-                var $nextButton = $(TEMPLATE.NEXT_BUTTON);
-                $this.on('click', String.format('#{0}', nextButtonId), function () {
-                    renderGrid(config, $this, page + 1);
-                });
-
-                $this.append($nextButton);
-            }
+            //$container.append(String.format(TEMPLATE.PAGE, page + 1));
         });
+    }
+
+    function renderHeader(config) {
+        for (col = 0; col < config.columns.length; col++) {
+            $thead.append(String.format(TEMPLATE.TH, config.columns[col]));
+        }
+    }
+
+    function renderBody(config, page) {
+        const pageSize = config.paging.pageSize;
+        const pageStart = page * pageSize;
+        const pageEnd = Math.min(pageStart + pageSize, config.data.length);
+
+        $tbody.empty();
+
+        for (row = pageStart; row < pageEnd; row++) {
+            var $row = $(TEMPLATE.TR);
+            for (col = 0; col < config.columns.length; col++) {
+                $row.append(String.format(TEMPLATE.TD, config.data[row][col]));
+            }
+            $tbody.append($row);
+        }
+    }
+
+    function renderPagingButtons(config, page) {
+        const pageSize = config.paging.pageSize;
+        const pageStart = page * pageSize;
+        const pageEnd = Math.min(pageStart + pageSize, config.data.length);
+
+        $pagingButtons.empty();
+
+        if (page > 0) {
+            var $previousButton = $(TEMPLATE.PREVIOUS_BUTTON);
+            $container.on('click', String.format('#{0}', previousButtonId), function () {
+                renderBody(config, page - 1);
+                renderPagingButtons(config, page - 1);
+            });
+
+            $pagingButtons.append($previousButton);
+        }
+
+        if (config.data.length > pageEnd) {
+            var $nextButton = $(TEMPLATE.NEXT_BUTTON);
+            $container.on('click', String.format('#{0}', nextButtonId), function () {
+                renderBody(config, page + 1);
+                renderPagingButtons(config, page + 1);
+            });
+
+            $pagingButtons.append($nextButton);
+        }
     }
 
 })(jQuery);
